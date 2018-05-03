51e3b06c07d8cdf5df897e4b543b27b82ac484ba
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-05-03 21:02:12 +0000

    automatically backed up by learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -5,8 +5,8 @@ export default function manageRestaurants(state, action) {
   switch (action.type) {
     case 'ADD_RESTAURANT':
       id++;
-      const restaurant = Object.assign({}, action.text, { id: id });
-      return { todos: state.todos.concat(todo) };
+      const restaurant = Object.assign({}, action.text, { id: cuid });
+      return { restaurants: state.restaurants.concat(restaurant) };
     case 'DELETE_RESTAURANT':
       return 'hello'
     default:
