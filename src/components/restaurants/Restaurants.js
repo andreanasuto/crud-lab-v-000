fd63499a13bc8e7e4c8984927de360862f882b6c
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-05-03 20:52:17 +0000

    automatically backed up by learn

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 
 class Restaurants extends Component {
   render() {
+    
     const restaurants = this.props.restaurants.map(function(restaurant){
       return <Restaurant restaurant={restaurant} />
     })
+
     return(
       <ul>
         {restaurants}
