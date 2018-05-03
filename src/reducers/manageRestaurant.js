578e0676ee6dc96bc6def45f87704ba9f6d92fe6
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-05-03 20:41:15 +0000

    automatically backed up by learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -5,7 +5,7 @@ export const cuidFn = cuid;
 export default function manageRestaurants(state, action) {
   switch (action.type) {
     case 'ADD_RESTAURANT':
-       state.store.restaurants.concat(action.text)
+      const restaurants = state.store.restaurants.concat(action.text)
        return {restaurants}
     default:
       return state
