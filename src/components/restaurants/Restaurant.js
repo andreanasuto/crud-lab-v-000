948c05c84ecedc218d7d2ab3ceda38ece2db6848
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-05-03 21:54:02 +0000

    automatically backed up by learn

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReviewInput from './components/reviews/ReviewInput'
+import ReviewInput from './reviews/ReviewInput'
 
 class Restaurant extends Component {
   handleClick = (event) => {
