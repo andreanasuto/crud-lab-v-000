85ccc5aae9959ad9b9e5591a17d09ecf97b4f43e
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-05-03 20:31:00 +0000

    automatically backed up by learn

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -4,8 +4,9 @@ class RestaurantInput extends Component {
   render() {
     return (
       <div>
+        <form>
           <input type="text">Your restaurant</input>
-          <input type="submit">Submit</input>
+        </form>
       </div>
     );
   }
