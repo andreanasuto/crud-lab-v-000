c11c379c3dbbf9c40190803f162afd977652278b
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-05-03 20:36:49 +0000

    automatically backed up by learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -3,5 +3,12 @@ import cuid from 'cuid';
 export const cuidFn = cuid;
 
 export default function manageRestaurants(state, action) {
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
+
+      break;
+    default:
+
+  }
 
 }
