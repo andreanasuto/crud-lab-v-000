d5aacdd352a53e4cd2c0852bbd22772ef60140dc
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-05-04 00:17:45 +0000

    automatically backed up by learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,14 @@ import ReviewInput from './ReviewInput';
 
 class Reviews extends Component {
   render() {
-    const reviews = this.props.getState().restaurants.filter(function(el){
-      return el.id = this.props.restaurantId
+    const filteredReviews = this.props.getState().restaurants.filter(function(el){
+       el.id = this.props.restaurantId
     })
+
+    const reviews = filteredReviews.map(function(el){
+      return <Review />
+    })
+
     return (
       <ul>
         {reviews}
