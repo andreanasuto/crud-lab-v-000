14f240d2b29e2629088dd6ea46c9f791f13327f2
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-05-04 00:15:59 +0000

    automatically backed up by learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,8 +3,12 @@ import ReviewInput from './ReviewInput';
 
 class Reviews extends Component {
   render() {
+    const reviews = this.props.getState().restaurants.filter(function(el){
+      return el.id = this.props.restaurantId
+    })
     return (
       <ul>
+        {reviews}
       </ul>
     );
   }
