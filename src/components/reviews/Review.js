bd0389ec5de574fe6ae2a9f17f1e279927a51552
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-05-04 00:52:02 +0000

    automatically backed up by learn

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -3,14 +3,17 @@ import React, { Component } from 'react';
 class Review extends Component {
   constructor () {
     super()
-
-    handleClick = () => {
-      this.props.store.dispatch({
-        action: 'DELETE_REVIEW',
-        id: this.props.review.id
-      })
-    }
+    
+    this.handleClick = this.handleClick.bind(this);
   }
+  
+  handleClick = () => {
+    this.props.store.dispatch({
+      action: 'DELETE_REVIEW',
+      id: this.props.review.id
+    })
+  }
+  
   render() {
     return (
       <li>
